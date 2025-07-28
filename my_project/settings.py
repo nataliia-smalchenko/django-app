a749ea54851ffe5e32a319a25c00cdeab6da0f9a
Nataliia Smalchenko <137941820+nataliia-smalchenko@users.noreply.github.com>
2025-07-28 13:01:38 +0300

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.postgresql',
 		'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
 		'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-		'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+		'NAME': os.environ.get('POSTGRES_DB', 'myapp'),
 		'USER': os.environ.get('POSTGRES_USER', 'postgres'),
 		'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
 	}
